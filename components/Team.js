ac14288f149a540c5b3b015c1076e1d837367bcb
danielboots <you@example.com>
2021-07-27 13:16:27 +0100

    updated linked in profile Kevin

--- components/Team.js
+++ components/Team.js
@@ -22,7 +22,7 @@ const people = [
     imageUrl: "/images/kevinscales.webp",
     bio: "Our team have over 20+ years of industry experience we pride ourselves on results and innovation.",
     twitterUrl: "#",
-    linkedinUrl: "#",
+    linkedinUrl: "https://www.linkedin.com/in/kevin-scales-53b87479/",
   },
   // More people...
 ];
@@ -79,7 +79,7 @@ export default function Team() {
                           </p>
                         </div>
                         <ul className="flex space-x-5">
-                          <li>
+                          {/* <li>
                             <a
                               href={person.twitterUrl}
                               className="text-gray-400 hover:text-gray-500"
@@ -94,10 +94,11 @@ export default function Team() {
                                 <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
                               </svg>
                             </a>
-                          </li>
+                          </li> */}
                           <li>
                             <a
                               href={person.linkedinUrl}
+                              target="_blank"
                               className="text-gray-400 hover:text-gray-500"
                             >
                               <span className="sr-only">LinkedIn</span>
