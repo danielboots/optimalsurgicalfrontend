6c8967dc1cb3b9ab8b5663ab5053a9cd8b1d6b71
danielboots <you@example.com>
2021-08-04 11:26:10 +0100

    hero animation on home text

--- components/Hero.js
+++ components/Hero.js
@@ -6,10 +6,10 @@ export default function Hero() {
   return (
     <div>
       <div className={styles.herohome}>
-        <h1 className=" font-body text-center font-bold z-20 tracking-wider text-3xl  sm:text-4xl md:text-6xl ">
+        <h1 className=" text-focus-in  font-body text-center font-bold z-20 tracking-wider text-3xl  sm:text-4xl md:text-6xl ">
           Optimal Surgical
         </h1>
-        <h2 className=" tracking-widest font-body text-center mt-5 z-20  text-2xl  sm:text-xl md:text-3xl  ">
+        <h2 className=" focus-in-contract-bck tracking-widest font-body text-center mt-5 z-20  text-2xl  sm:text-xl md:text-3xl  ">
           Professional Medical Device Services.
         </h2>
       </div>
